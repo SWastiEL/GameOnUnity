102e6d8b79c890e1e876640a67c6b87fc9291baa
SWastiEL <slowkingart@gmail.com>
2020-06-07 12:46:59 +0700

    Фон, Создан playerCtrl

---
+++ Space/Assets/_Screpts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    void FixedUpdate()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0f);
+        rb.velocity = movement*speed;
+    }
+}
