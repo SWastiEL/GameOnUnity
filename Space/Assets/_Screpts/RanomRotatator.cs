8d7f564a94af82719dc9a54d2b325235dfc4957a
SWastiEL <slowkingart@gmail.com>
2020-06-07 16:54:21 +0700

    Добавлены астероиды в каталог, скрипт вращения астероида

---
+++ Space/Assets/_Screpts/RanomRotatator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RanomRotatator : MonoBehaviour
+{
+    public float tumble;
+
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
