9ac6b7f5c79a2d2126242ca6cef11ee7debe8536
SWastiEL <slowkingart@gmail.com>
2020-06-07 17:05:49 +0700

    DestroyByContact screept

---
+++ Space/Assets/_Screpts/DestroyByContact.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag=="Boundary")
+        {
+            return;
+        }
+        Destroy(collider.gameObject);
+        Destroy(gameObject);
+    }
+}
