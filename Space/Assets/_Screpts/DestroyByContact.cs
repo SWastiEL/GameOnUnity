883bb218293eea54cb3b8c0bdf6a099fa04932b9
SWastiEL <slowkingart@gmail.com>
2020-06-08 19:58:48 +0700

    Префаб астероида с движением

--- Space/Assets/_Screpts/DestroyByContact.cs
+++ Space/Assets/_Screpts/DestroyByContact.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
+    //public GameObject playerExplosion;
+
     void OnTriggerEnter(Collider collider)
     {
         if (collider.tag=="Boundary")
         {
             return;
         }
+        Instantiate(explosion, transform.position, transform.rotation);
+        //if (collider.tag=="Player")
+        //{
+        //    Instantiate(playerExplosion, collider.transform.position, collider.transform.rotation);
+        //}
         Destroy(collider.gameObject);
         Destroy(gameObject);
     }
